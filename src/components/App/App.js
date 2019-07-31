be76975cac9ae7e0ffd8a3de03bd72393748b316
Kevin <brackkevin@gmail.com>
2019-07-31 12:22:01 -0500

    Moved URL and fetchResults into useEffect. Adds params state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,22 +4,23 @@ import axios from "axios";
 const KEY = process.env.REACT_APP_PIXABAY_KEY;
 
 const App = () => {
-  const URL = `https://pixabay.com/api/?key=${KEY}`;
   const [data, setData] = useState([]);
-
-  const fetchResults = () => {
-    axios
-      .get(URL)
-      .then(res => {
-        console.log("RES: ", res);
-        setData(res.data.hits);
-      })
-      .catch(err => console.log(err));
-  };
+  const [params, setParams] = useState("");
 
   useEffect(() => {
+    const URL = `https://pixabay.com/api/?key=${KEY}${params}`;
+    const fetchResults = () => {
+      axios
+        .get(URL)
+        .then(res => {
+          console.log("RES: ", res);
+          setData(res.data.hits);
+        })
+        .catch(err => console.log(err));
+    };
     fetchResults();
-  }, []);
+    return;
+  }, [params]);
 
   return <div>{data.length > 0 ? "DATA RECIEVED" : "NOT YET"}</div>;
 };

305fc13c11990a62409413399d4158225fefe799
Kevin <brackkevin@gmail.com>
2019-07-31 11:48:48 -0500

    Retreiving data from pixabay api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
+import axios from "axios";
+const KEY = process.env.REACT_APP_PIXABAY_KEY;
 
-function App() {
-  return <div>APP</div>;
-}
+const App = () => {
+  const URL = `https://pixabay.com/api/?key=${KEY}`;
+  const [data, setData] = useState([]);
+
+  const fetchResults = () => {
+    axios
+      .get(URL)
+      .then(res => {
+        console.log("RES: ", res);
+        setData(res.data.hits);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
+
+  return <div>{data.length > 0 ? "DATA RECIEVED" : "NOT YET"}</div>;
+};
 
 export default App;

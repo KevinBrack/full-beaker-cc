3467f3d2cd773a6080f5a053217d0037dbbbed78
Kevin <brackkevin@gmail.com>
2019-07-31 16:29:57 -0500

    Added categories and initial form fields

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -2,7 +2,48 @@ import React from "react";
 
 const InputForm = props => {
   const { setParams } = props;
-  return <div>INPUT FORM</div>;
+  const categories = [
+    "",
+    "fashion",
+    "nature",
+    "backgrounds",
+    "science",
+    "education",
+    "people",
+    "feelings",
+    "religion",
+    "health",
+    "places",
+    "animals",
+    "industry",
+    "food",
+    "computer",
+    "sports",
+    "transportation",
+    "travel",
+    "buildings",
+    "business",
+    "music"
+  ];
+  return (
+    <div>
+      INPUT FORM
+      <form>
+        <div>
+          <label htmlFor="search-form" />
+          <input type="text" id="search-form" name="search-form" />
+        </div>
+        <div>
+          <label htmlFor="category-select" />
+          <select id="category-select">
+            {categories.map(category => {
+              return <option key={category}>{category}</option>;
+            })}
+          </select>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default InputForm;

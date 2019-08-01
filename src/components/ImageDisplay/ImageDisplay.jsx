33ddaa8373f11db61beced56810524fa46d3fa61
Kevin Brack <brackkevin@gmail.com>
2019-07-31 21:40:44 -0500

    Looped over and displayed image previews

--- src/components/ImageDisplay/ImageDisplay.jsx
+++ src/components/ImageDisplay/ImageDisplay.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 const ImageDisplay = props => {
   const { data } = props;
-  return <div>IMAGE DISPLAY</div>;
+  return (
+    <div>
+      {data.map(pic => {
+        return (
+          <a href={pic.largeImageURL} key={pic.id}>
+            <img src={pic.previewURL} alt={pic.tags} />
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ImageDisplay;

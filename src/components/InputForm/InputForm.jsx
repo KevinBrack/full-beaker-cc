82010e11c6ec2dc7a3dafde5a830a6e4ada668c8
Kevin Brack <brackkevin@gmail.com>
2019-07-31 21:23:52 -0500

    Set form up and creates query on submit

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputForm = props => {
   const { setParams } = props;
+  const [searchString, setSearchString] = useState("");
+  const [searchCategory, setSearchCategory] = useState("");
   const categories = [
     "",
     "fashion",
@@ -25,22 +27,51 @@ const InputForm = props => {
     "business",
     "music"
   ];
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let params = "";
+    if (searchString) {
+      let search = searchString;
+      search = search
+        .trim()
+        .split(" ")
+        .join("&");
+      params += `&q=${search}`;
+    }
+    if (searchCategory) {
+      params += `&category=${searchCategory}`;
+    }
+    setParams(params);
+  };
+
   return (
     <div>
-      INPUT FORM
-      <form>
+      SEARCHING FOR {searchString.toUpperCase()} IN CATEGORY{" "}
+      {searchCategory.toUpperCase()}
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="search-form" />
-          <input type="text" id="search-form" name="search-form" />
+          <input
+            type="text"
+            id="search-form"
+            value={searchString}
+            onChange={e => setSearchString(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="category-select" />
-          <select id="category-select">
+          <select
+            id="category-select"
+            value={searchCategory}
+            onChange={e => setSearchCategory(e.target.value)}
+          >
             {categories.map(category => {
               return <option key={category}>{category}</option>;
             })}
           </select>
         </div>
+        <button type="submit">SEARCH</button>
       </form>
     </div>
   );

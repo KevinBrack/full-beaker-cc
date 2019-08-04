2b4b771d4063f284548fd70475a3a08f45944a69
Kevin Brack <brackkevin@gmail.com>
2019-08-03 22:51:42 -0500

    Passing fav methods to Thumbnail component

--- src/components/ImageDisplay/ImageDisplay.jsx
+++ src/components/ImageDisplay/ImageDisplay.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Thumbnail from "./Thumbnail";
 
 const ImageDisplay = props => {
-  const { data } = props;
+  const { data, favorites, delFav, addFav } = props;
   return (
     <div>
       {data.map(pic => {
         return (
-          <a href={pic.largeImageURL} key={pic.id}>
-            <img src={pic.previewURL} alt={pic.tags} />
-          </a>
+          <Thumbnail
+            pic={pic}
+            key={pic.id}
+            favorites={favorites}
+            delFav={delFav}
+            addFav={addFav}
+          />
         );
       })}
     </div>

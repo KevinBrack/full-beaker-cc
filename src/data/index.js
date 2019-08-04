21d7d158339174b52d952b7d1b24093bc94cb6a2
Kevin Brack <brackkevin@gmail.com>
2019-08-04 11:00:23 -0500

    Moved categories to data file

---
+++ src/data/index.js
@@ -0,0 +1,23 @@
+export const categories = [
+  "",
+  "fashion",
+  "nature",
+  "backgrounds",
+  "science",
+  "education",
+  "people",
+  "feelings",
+  "religion",
+  "health",
+  "places",
+  "animals",
+  "industry",
+  "food",
+  "computer",
+  "sports",
+  "transportation",
+  "travel",
+  "buildings",
+  "business",
+  "music"
+].sort();

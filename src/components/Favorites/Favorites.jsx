e7718bfe558101a435616f1361f7a772479d2988
Kevin Brack <brackkevin@gmail.com>
2019-08-03 23:14:31 -0500

    Mapped over favorite object keys and installed fav display

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Favorite from "./Favorite";
 
 const Favorites = props => {
   const { favorites, delFav } = props;
-  return <div>FAVORITES</div>;
+  return (
+    <div>
+      {Object.keys(favorites).map(fav => {
+        return <Favorite favorite={fav} delFav={delFav} />;
+      })}
+    </div>
+  );
 };
 
 export default Favorites;

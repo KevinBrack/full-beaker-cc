202327ac06674b9ec72a5cddedddf5dff3490002
Kevin Brack <brackkevin@gmail.com>
2019-08-03 22:50:45 -0500

    Implemented favorites in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,16 @@ import "./App.scss";
 import axios from "axios";
 import InputForm from "../InputForm/InputForm";
 import ImageDisplay from "../ImageDisplay/ImageDisplay";
+import Favorites from "../Favorites/Favorites";
+import useFav from "../../hooks/useFav";
 const KEY = process.env.REACT_APP_PIXABAY_KEY;
 
 const App = () => {
   const [data, setData] = useState([]);
   const [params, setParams] = useState("&q=puppies");
+  const { favorites, addFav, delFav } = useFav();
 
+  // AXIOS REQUEST TO QUERY PIXABAY
   useEffect(() => {
     const URL = `https://pixabay.com/api/?key=${KEY}${params}`;
     const fetchResults = () => {
@@ -28,7 +32,13 @@ const App = () => {
       <h1>{data.length > 0 ? "DATA RECIEVED" : "NOT YET"}</h1>
       <h2>params: {params}</h2>
       <InputForm setParams={setParams} />
-      <ImageDisplay data={data} />
+      <ImageDisplay
+        data={data}
+        favorites={favorites}
+        delFav={delFav}
+        addFav={addFav}
+      />
+      <Favorites favorites={favorites} delFav={delFav} />
     </div>
   );
 };

5effbb763cfcd0d19b68aaa3a775186ce90cb40e
Kevin Brack <brackkevin@gmail.com>
2019-08-04 11:26:13 -0500

    Cleaned out debugging console logs

--- src/hooks/useFav.jsx
+++ src/hooks/useFav.jsx
@@ -6,21 +6,21 @@ const useFav = () => {
   const [favorites, setFavorites] = useState(initialFavs);
 
   const addFav = fav => {
-    console.log(`IN HOOK SETTING ${fav}`);
+    // console.log(`IN HOOK SETTING ${fav}`);
     const newFav = { ...favorites };
     newFav[fav] = true;
     setFavorites(newFav);
   };
 
   const delFav = fav => {
-    console.log(`IN HOOK REMOVING ${fav}`);
+    // console.log(`IN HOOK REMOVING ${fav}`);
     const newFav = { ...favorites };
     delete newFav[fav];
     setFavorites(newFav);
   };
 
   useEffect(() => {
-    console.log("SETTING LOCAL FAVORITES");
+    // console.log("SETTING LOCAL FAVORITES");
     localStorage.setItem("localFav", JSON.stringify(favorites));
   }, [favorites]);
 

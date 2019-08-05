8bf9497789af09d012d53e826787aba79ea6d34d
Kevin <brackkevin@gmail.com>
2019-08-04 21:52:44 -0500

    Standardized image dimentions

--- src/components/ImageDisplay/Thumbnail/Thumbnail.jsx
+++ src/components/ImageDisplay/Thumbnail/Thumbnail.jsx
@@ -1,19 +1,28 @@
 import React, { Fragment } from "react";
+import styled from "styled-components";
 
 const Thumbnail = props => {
   const { pic } = props;
 
+  const Background = styled.div`
+  background-image: url("${pic.previewURL}");
+  background-position: center;
+  background-size: cover;
+  height: 99px;
+  width: 150px;
+  `;
+
   return (
-    <Fragment>
-      <a
+    <Background>
+      {/* <a
         href={pic.largeImageURL}
         key={pic.id}
         target="_blank"
         rel="noopener noreferrer"
-      >
-        <img src={pic.previewURL} alt={pic.tags} />
-      </a>
-    </Fragment>
+      > */}
+      {/* <img src={pic.previewURL} alt={pic.tags} /> */}
+      {/* </a> */}
+    </Background>
   );
 };
 

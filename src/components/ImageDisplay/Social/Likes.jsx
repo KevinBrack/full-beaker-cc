ab545d740615ca101a605622fe0a00b39df17bdf
Kevin <brackkevin@gmail.com>
2019-08-04 22:52:02 -0500

    Social icons color toggle and clickable

--- src/components/ImageDisplay/Social/Likes.jsx
+++ src/components/ImageDisplay/Social/Likes.jsx
@@ -1,25 +1,55 @@
-import React, { useState } from "react";
-import { Star, Thumb } from "../../Icons/index";
+import React, { useState, useContext } from "react";
+import { Star, StarActive, Thumb, ThumbActive } from "../../Icons/index";
 import "./Likes.scss";
+import { AppContext } from "../../App/AppContext";
 const Likes = props => {
   const { pic } = props;
+  const { favorites, addFav, delFav } = useContext(AppContext);
   const [liked, setLiked] = useState(false);
 
   const handleLike = () => {
     setLiked(!liked);
   };
 
+  const handleFav = () => {
+    if (favorites[pic.id]) {
+      delFav(pic);
+    } else {
+      addFav(pic);
+    }
+  };
+
+  const ThumbSwitch = () => {
+    if (liked) {
+      return <ThumbActive />;
+    } else {
+      return <Thumb />;
+    }
+  };
+
+  const StarSwitch = () => {
+    if (favorites[pic.id]) {
+      return <StarActive />;
+    } else {
+      return <Star />;
+    }
+  };
+
   return (
     <div className="social-container">
       <div className="social-likes">
-        {liked ? pic.likes + 1 : pic.likes}
+        <span className="social-text">{liked ? pic.likes + 1 : pic.likes}</span>
         <div onClick={handleLike}>
-          <Thumb />
+          <ThumbSwitch />
         </div>
       </div>
       <div className="social-favorites">
-        {pic.favorites}
-        <Star />
+        <span className="social-text">
+          {favorites[pic.id] ? pic.favorites + 1 : pic.favorites}
+        </span>
+        <div onClick={handleFav}>
+          <StarSwitch />
+        </div>
       </div>
     </div>
   );

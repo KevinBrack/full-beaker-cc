7735105f5474fcc7091b525756259f24c5b1d6a2
Kevin <brackkevin@gmail.com>
2019-08-05 00:01:23 -0500

    Fix header widths

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Header.scss";
 
 const Header = props => {
-  return <div>HEADER</div>;
+  return (
+    <div className="header-bg">
+      <div className="header-container">
+        <h1>Stock Image Search</h1>
+        <ul>
+          <a href="https://kevinbrack.com">
+            <li>by Kevin Brack</li>
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

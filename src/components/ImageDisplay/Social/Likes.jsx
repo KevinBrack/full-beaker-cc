5c62cc281ed3af1270e5dcf017ac52a015d561d5
Kevin <brackkevin@gmail.com>
2019-08-04 22:20:55 -0500

    Faux like functionality

--- src/components/ImageDisplay/Social/Likes.jsx
+++ src/components/ImageDisplay/Social/Likes.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Star, Thumb } from "../../Icons/index";
 import "./Likes.scss";
 const Likes = props => {
   const { pic } = props;
-  // console.log("PIC: ", pic);
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className="social-container">
       <div className="social-likes">
-        {pic.likes}
-        <Thumb />
+        {liked ? pic.likes + 1 : pic.likes}
+        <div onClick={handleLike}>
+          <Thumb />
+        </div>
       </div>
       <div className="social-favorites">
         {pic.favorites}

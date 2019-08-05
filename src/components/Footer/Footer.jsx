44f1f7e1422f54c7b4e2bf2fb0bef37415396fa1
Kevin <brackkevin@gmail.com>
2019-08-04 23:34:00 -0500

    Footer created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = props => {
-  return <div>FOOTER</div>;
+  return (
+    <div className="footer-container">
+      <div>Search results provided by Pixabay</div>
+      <a href="https://pixabay.com/">
+        <img
+          src="https://pixabay.com/static/img/public/leaderboard_a.png"
+          alt="Pixabay"
+        />
+      </a>
+      <div>©2019 Kevin Brack</div>
+    </div>
+  );
 };
 
 export default Footer;

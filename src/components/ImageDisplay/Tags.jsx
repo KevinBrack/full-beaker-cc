6f896f4a489afc90d99f2ca7742e79ac3a430f65
Kevin <brackkevin@gmail.com>
2019-08-04 19:13:57 -0500

    Tags map and styling

--- src/components/ImageDisplay/Tags.jsx
+++ src/components/ImageDisplay/Tags.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './Tags.scss';
 
 const Tags = props => {
   const {pic} = props;
-  return <div>TAGS</div>
+  const tagsArr = pic.tags.split(", ")
+  console.log("TAGS: ", tagsArr);
+  const tagsDisplay = tagsArr.map(tag => {
+    return <div className="tag">{tag}</div>
+  })
+  return <div className="tag-container">{tagsDisplay}</div>;
 }
 
 export default Tags;

214e2a29c3f3ea6fb59e60efdcf8dc3d079d2c6f
Kevin <brackkevin@gmail.com>
2019-08-04 21:38:18 -0500

    Made tags generate a new search

--- src/components/ImageDisplay/Tags/Tags.jsx
+++ src/components/ImageDisplay/Tags/Tags.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Tags.scss";
+import { AppContext } from "../../App/AppContext";
 
 const Tags = props => {
+  const { setParams } = useContext(AppContext);
   const { pic } = props;
   const tagsArr = pic.tags.split(", ");
-  // console.log("TAGS: ", tagsArr);
+
+  const handleTagSelect = tag => {
+    const params = `&q=${tag}`;
+    setParams(params);
+  };
+
   const tagsDisplay = tagsArr.map(tag => {
     return (
-      <div className="tag" key={tag}>
+      <div className="tag" key={tag} onClick={() => handleTagSelect(tag)}>
         <span>{tag}</span>
       </div>
     );
